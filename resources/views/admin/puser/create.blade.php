8bdc9b85839b5e5ac19fa82d132e9a07edd3a71b
zz <aaaa@example.com>
2020-10-08 08:07:57 +0800

    提交4

--- resources/views/admin/puser/create.blade.php
+++ resources/views/admin/puser/create.blade.php
@@ -1,34 +1,44 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 <form action="{{url('save')}}" method="post">
     @csrf
     <table>
         <tr>
             <td>用户名</td>
-            <td><input type="text" name="user_name"></td>
+            <td>
+                <input type="text" name="user_name" id="">
+            </td>
         </tr>
         <tr>
-            <td>邮箱</td>
-            <td><input type="text" name="email"></td>
+            <td>邮箱名</td>
+            <td>
+                <input type="text" name="email" id="">
+            </td>
         </tr>
         <tr>
             <td>密码</td>
-            <td><input type="password" name="password"></td>
+            <td>
+                <input type="password" name="password" id="">
+            </td>
         </tr>
         <tr>
+            <td><input type="submit" value="注册"></td>
             <td></td>
-            <td><input type="submit" value="登陆"></td>
         </tr>
-
     </table>
 </form>
-</body>
-</html>
