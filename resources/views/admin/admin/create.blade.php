c9b1a2810873c9252b39252cf5b50ef12f81e92a
zz <aaaa@example.com>
2020-10-08 08:06:47 +0800

    提交3

--- resources/views/admin/admin/create.blade.php
+++ resources/views/admin/admin/create.blade.php
@@ -10,19 +10,15 @@
         @csrf
     <table>
         <tr>
-            <td>姓名</td>
+            <td>登录</td>
             <td><input type="text" name='admin_name'></td>
         </tr>
         <tr>
-            <td>年龄</td>
+            <td>密码</td>
             <td><input type="text"name='admin_pwd'></td>
         </tr>
         <tr>
-            <td>手机号</td>
-            <td><input type="text"name='admin_tel'></td>
-        </tr>
-        <tr>
-            <td><input type="submit" value="提交"></td>
+            <td><input type="submit" value="登录"></td>
             <td></td>
         </tr>
     </table>
