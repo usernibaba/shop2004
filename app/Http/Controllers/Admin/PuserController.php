1b9e701662b9d12d1d9ba4683e798add2069f7f8
zz <aaaa@example.com>
2020-10-08 08:00:00 +0800

    提交

--- app/Http/Controllers/Admin/PuserController.php
+++ app/Http/Controllers/Admin/PuserController.php
@@ -13,15 +13,40 @@ class PuserController extends Controller
 
     public  function  save(Request $request){
         $data=$request->except('_token');
+//        dd($data);
         $data['reg_time']=time();
         $res=DB::table('puser')->insert($data);
         if($res){
-            return redirect('PuserController/index');
+            return redirect('login');
         }
     }
     public  function  index(){
         $res=DB::table('puser')->get();
         return view('admin/puser/index',['res'=>$res]);
+    }
+    //登录页面
+    function login(){
+        return view('admin/puser/login');
+}
+    function logindo(Request $request){
+        $user=$request->except('_token');
+//         dd($user);
+        //根据查询表中数据
+        $username=DB::table('puser')->where('user_name',$user['user_name'])->first();
+        // dd($username);
+        //验证账号是否与表中一制
+        if(!$username){
+            return redirect('/login')->with('msg','没有此用户');
+        }
+        //判断密码是否正确
+        if($username->password!=$user['password']){
+            return redirect('/login')->with('msg','密码错误');
+        }
+        //存入登录时间
+
+        DB::table('puser')->where('uid',$username->uid)->update($user);
+        //跳转到展示页面
+        return redirect('index');
     }
 
 }
